7cacd2d91551cac2b098414d801049caa15c7b84
jorgedf1991 <jorgeldiaz1628@gmail.com>
2023-06-05 21:19:01 -0500

    agregado cors y configuracion

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const cors = require('cors');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(session({ secret : 'top secret', resave: false, saveUninitialized: false }));
+app.use(cors());
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './views'));
