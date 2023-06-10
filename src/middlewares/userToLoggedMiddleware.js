b3438b939db64368bd7ab9f30419df157477dcde
jorgedf1991 <jorgeldiaz1628@gmail.com>
2023-06-10 18:10:01 -0500

    cambios middleware cookie

--- src/middlewares/userToLoggedMiddleware.js
+++ src/middlewares/userToLoggedMiddleware.js
@@ -1,29 +1,18 @@
 const db = require('../database/models');
 
 function userLoggedMiddleware(req, res, next) {
-    // res.locals.isLogged = !!req.session.userLogged;
-
-    // if (req.session.userLogged) {
-    //     res.locals.isLogged = true
-    // }else{
-    //     res.locals.isLogged = false
-    // }
-
-    // res.locals.isLogged = req.session.userLogged ? true : false
-
-    let emailInCookie = req.cookies.userEmail;
-   
-    if (emailInCookie) {
-        db.User.findOne({ where: { email: emailInCookie } })
-            .then(userFromCookie => {
-                if (userFromCookie) {
-                    req.session.userLogged = userFromCookie;
-                }
-
-                if (req.session && req.session.userLogged) {
-                    res.locals.isLogged = true;
-                }
-
+            
+            let emailInCookie = req.cookies.userEmail;
+            
+            if (emailInCookie) {
+                db.User.findOne({ where: { email: emailInCookie } })
+                .then(userFromCookie => {
+                    if (userFromCookie) {
+                        req.session.userLogged = userFromCookie;
+                    }
+                    
+                    res.locals.isLogged = !!req.session.userLogged;
+                
                 next();
             })
             .catch(err => {
