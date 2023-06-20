9366623426b6298190d3bbc3a144a0092f1f8296
FranBudde <buddemeyerizzo@gmail.com>
2023-06-20 02:00:19 -0300

    apis de search arregladas y funcionando

--- src/routes/api/productsApi.js
+++ src/routes/api/productsApi.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productsApi = require('../../contrrollers/api/productsApi');
 
 router.get('/product', productsApi.products);
-router.get('/product/:id', productsApi.detail);
-router.get('/user/search', productsApi.search);
+router.get('/product/search', productsApi.search);
+router.get('/product/:id', productsApi.detail); //deja esto aca, porque sino el :id interfiere con el search, el id siempre va ultimo
 
 module.exports = router;
--- src/routes/api/userApi.js
+++ src/routes/api/userApi.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const apiUser = require('../../contrrollers/api/apiUser');
 
 router.get('/user', apiUser.user);
+router.get('/users/search', apiUser.search);
 router.get('/user/:id', apiUser.detail);
+
 
 module.exports = router;
